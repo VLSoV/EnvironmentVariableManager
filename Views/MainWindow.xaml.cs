1c09eedd5d69827294896029f75e2357b0704bcf
akvatub <akvatub@gmail.com>
2026-05-15 03:53:36 +0300

    remove buttons refresh and save

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using EnvManager.Models;
-using EnvManager.Services;
-using EnvManager.ViewModels;
-using System.Collections.ObjectModel;
+﻿using EnvManager.ViewModels;
 using System.Windows;
 using System.Windows.Input;
 
@@ -9,52 +6,10 @@ namespace EnvManager.Views;
 
 public partial class MainWindow : Window
 {
-    private readonly EnvironmentService _envService;
-    private readonly CommentStorageService _commentStorageService;
-    private readonly MainViewModel _mainViewModel;
-    private ObservableCollection<EnvironmentVariable> Variables { get; set; }
-
-    public MainWindow(EnvironmentService envService,
-        CommentStorageService commentStorageServicel,
-        MainViewModel mainViewModel)
+    public MainWindow(MainViewModel mainViewModel)
     {
         InitializeComponent();
-        _envService = envService;
-        _commentStorageService = commentStorageServicel;
-        _mainViewModel = mainViewModel;
-        Variables = _mainViewModel.Variables;
-        VariablesGrid.ItemsSource = Variables;
-        Loaded += (s, e) => _mainViewModel.LoadData();
-
         DataContext = mainViewModel;
-    }
-
-    private void SaveButton_Click(object sender, RoutedEventArgs e)
-    {
-        try 
-        {
-            foreach (var variable in Variables)
-            {
-                _envService.SetVariable(variable);
-                _commentStorageService.SetComment(variable);
-            }
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show(ex.Message);
-        }
-    }
-
-    private void RefreshButton_Click(object sender, RoutedEventArgs e)
-    {
-        try
-        {
-            _mainViewModel.LoadData();
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show(ex.Message);
-        }
     }
 
     // Обработчики для кастомного окна
